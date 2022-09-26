1d7de547ab26ce90aedfb1303a856253bb79a3c5
niasbezerra <bezerranias@gmail.com>
2022-09-26 15:43:39 -0300

    atualizacao

--- maldivastravel/src/main/java/model/DAO.java
+++ maldivastravel/src/main/java/model/DAO.java
@@ -1,5 +1,21 @@
 package model;
 
-public class DAO {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class DAO {
+	
+	public static Connection getConnection() {
+		Connection con = null;
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/bd_maldivastravel", "root","88147320");
+					
+	}catch (Exception e) {
+		System.out.println (e);
+		
+	}                   
+		return con;
+}
 }
